379412aa8ec29dfd8a865d7d59c0045f55e43379
awfyuun <awfyuun@gmail.com>
2025-03-28 16:55:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-var swiper = new Swiper(".mySwiper-1", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-  rotate: 50,
-  stretch: 0,
-  depth: 100,
-  modifier: 1,
-  slideShadows: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
-var swiper = new Swiper(".mySwiper-2", {
-  effect: "cards",
-  grabCursor: true,
-});
-
 // catrgory-swiper
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
@@ -31,23 +9,24 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 // SideBar
-    function sideBar__show() {
-    $('.mobile-sideBar').addClass('active'); 
-    }
+function sideBar__show() {
+  $(".mobile-sideBar").addClass("active");
+  $("html, body").css("overflow", "hidden");
+}
 
-    function sideBar__hide() {
-    $('.mobile-sideBar').removeClass('active'); 
-    }
+function sideBar__hide() {
+  $(".mobile-sideBar").removeClass("active");
+  $("html, body").css("overflow", "visible");
+}
 
+function sideBar__init() {
+  $(".btn_sideBar").click(function () {
+    sideBar__show();
+  });
 
-    function sideBar__init() {
-    $('.btn_sideBar').click(function() {
-        sideBar__show();
-    });
-    
-    $('.clear').click(function() {
-        sideBar__hide();
-    });
-    }
+  $(".clear").click(function () {
+    sideBar__hide();
+  });
+}
 
-    sideBar__init();
+sideBar__init();
